932c956f6ae845d6e0b57fca0b999ce0b6d3d34b
ShimulChandraDas <msshimul99@gmail.com>
2022-05-02 18:28:29 +0600

    adding steps

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 //const { request } = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -17,6 +18,27 @@ app.use(express.json());
 //XduLPLfquipwjvQ3  
 
 
+//require('crypto').randomBytes(64).toString('hex')
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(4010).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next()
+    })
+    //console.log('inside verifyJWT ', authHeader);
+
+}
+
+
 const uri = `mongodb://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0-shard-00-00.fh48o.mongodb.net:27017,cluster0-shard-00-01.fh48o.mongodb.net:27017,cluster0-shard-00-02.fh48o.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-cqsegg-shard-0&authSource=admin&retryWrites=true&w=majority`;
 //console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -26,8 +48,18 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
         console.log('Genius Car Db running');
+        const orderCollection = client.db('geniusCar').collection('order');
 
+        //AUTH
+        app.post('/login', async (res, req) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
+        //service API
         app.get('/service', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
@@ -55,6 +87,30 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        //order collection api
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            //console.log(email);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
+
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
     }
